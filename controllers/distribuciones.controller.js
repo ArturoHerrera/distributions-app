4326a48799d20fea2138484f2304212735ad1357
ArturoHerrera <joseherreravi92@gmail.com>
2020-07-07 22:36:08 -0600

    Se ordena codigo de distribucion uniforme

--- controllers/distribuciones.controller.js
+++ controllers/distribuciones.controller.js
@@ -1,12 +1,12 @@
 const distribucionesController = {
-  unirforme( req, res ) {
-
-    let cantidadElementos = null;
-    var result = null;
-    var resultArray = [];
-    var customResponse = null;
+  uniforme( req, res ) {
 
     try {
+      let cantidadElementos = null;
+      var result = null;
+      let resultArray = [];
+      let customResponse = null;
+
       cantidadElementos = req.body.cantidadElementos;
       result = ( 1 / cantidadElementos * 100 ).toFixed( 4 ) + '%';
 
@@ -19,14 +19,37 @@ const distribucionesController = {
           cantidadObjetos : resultArray
       }
 
-      res.send( { "customResponse" : customResponse } );
+      res.status( 200 ).send( { "customResponse" : customResponse } );
     } catch ( error ) {
       res.status( 500 ).send( error );
     }
 
   },
   binomial(req, res) {
-    //TODO
+    try {
+      let result,x,y = 0;
+            let diff = ciclos - n;
+
+            for (var i = 1; i <= n; i++) {
+              if (i == 1) {
+                x = valX;
+              } else {
+                x = x * valX;
+              }
+            }
+
+            for (var i = 1; i <= diff; i++) {
+              if (i == 1) {
+                y = valY;
+              } else {
+                y = y * valY;
+              }
+            }
+
+            return x * y;
+    } catch (error) {
+      res.status( 500 ).send( error );
+    }
   },
   poisson(req, res) {
     //TODO
--- routes/distribuciones.route.js
+++ routes/distribuciones.route.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var distribucionesController = require('../controllers/distribuciones.controller')
 
 /* GET users listing. */
-router.post('/unirforme', distribucionesController.unirforme);
+router.post('/uniforme', distribucionesController.uniforme);
+router.post('/binomial', distribucionesController.binomial);
 
 module.exports = router;
